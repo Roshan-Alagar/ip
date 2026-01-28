3ca8051f837a11b5222d2acdc623372c0a5815b6
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-01-28 20:39:10 +0800

    no message

---
+++ src/main/java/Roshan.java
@@ -0,0 +1,16 @@
+public class Roshan {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+         */
+        String logo = "Roshan";
+
+        System.out.println("Hello I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println(" ");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
