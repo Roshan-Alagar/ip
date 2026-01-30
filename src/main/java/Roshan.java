3971fbf11e299d696fa0bbbd2222cd23c30dd456
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-01-30 13:27:18 +0800

    Level 2 Tasks

--- src/main/java/Roshan.java
+++ src/main/java/Roshan.java
@@ -11,16 +11,34 @@ public class Roshan {
         System.out.println("Hello I'm Roshan" );
         System.out.println("What can I do for you?");
         System.out.println(line);
-        String repeat = args[0];
-        System.out.println(" ");
-        if(repeat.equals("bye"))
-        {
-            System.out.println("Bye. Hope to see you again soon!");
+
+        String[] tasks = new String[100];
+        int counter = 0;
+
+        for(int i = 0; i < args.length; i++) {
+            String repeat = args[i];
+            System.out.println(line);
+            if(repeat.equals("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+            }
+            else if(repeat.equals("list"))
+            {
+                for(int j = 0; j < counter; j++ )
+                {
+                    System.out.println("     " + (j + 1) + ". " + tasks[j]);
+                }
+                System.out.println(line);
+            }
+            else
+            {
+                tasks[counter] = repeat;
+                counter++;
+                System.out.println("     added: " + repeat);
+                System.out.println(line);
+            }
+            System.out.println(" ");
         }
-        else
-        {
-            System.out.println(repeat);
-        }
-        System.out.println(line);
     }
 }
