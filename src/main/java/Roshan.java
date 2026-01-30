b41b02abf296543ffa627788314cba215a116f77
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-01-30 12:59:33 +0800

    Level 1 Tasks

--- src/main/java/Roshan.java
+++ src/main/java/Roshan.java
@@ -5,10 +5,22 @@ public class Roshan {
                 + "| |_) / _ \\/ __| '_ \\ / _` | '_ \\ \n"
                 + "|  _ < (_) \\__ \\ | | | (_| | | | |\n"
                 + "|_| \\_\\___/|___/_| |_|\\__,_|_| |_|\n";
+        String line = " ____________________________________________________________";
         System.out.println(logo);
+        System.out.println(line);
         System.out.println("Hello I'm Roshan" );
         System.out.println("What can I do for you?");
+        System.out.println(line);
+        String repeat = args[0];
         System.out.println(" ");
-        System.out.println("Bye. Hope to see you again soon!");
+        if(repeat.equals("bye"))
+        {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else
+        {
+            System.out.println(repeat);
+        }
+        System.out.println(line);
     }
 }
