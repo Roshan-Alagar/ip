985101e0f108799ac75a67071c1736cd456892c1
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-03-03 19:17:39 +0800

    Add JavaDoc comments to Event class

--- src/main/java/roshan/Event.java
+++ src/main/java/roshan/Event.java
@@ -1,14 +1,32 @@
 package roshan;
 
+/**
+ * Represents an event task with start and end times.
+ */
+
 public class Event extends Task {
     protected String from;
     protected String to;
+
+    /**
+     * Creates a new Event task with the given description and time period.
+     *
+     * @param description The description of the event.
+     * @param from The start time of the event.
+     * @param to The end time of the event.
+     */
 
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
+
+    /**
+     * Returns the string representation of this event task.
+     *
+     * @return String with [E] prefix, status, description, and time period.
+     */
 
     @Override
 
