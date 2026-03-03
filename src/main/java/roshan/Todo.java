c317a62ee4a172f317fb38f98af8e0e5e8adabae
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-03-03 19:09:02 +0800

    Add JavaDoc comments to Todo class and fixed Coding Standard

--- src/main/java/roshan/Todo.java
+++ src/main/java/roshan/Todo.java
@@ -1,14 +1,32 @@
 package roshan;
 
+/**
+ * Represents a todo task without any date/time.
+ */
+
 public class Todo extends Task {
 
+    /**
+     * Creates a new Todo task with the given description.
+     *
+     * @param description The description of the todo task.
+     */
+
     public Todo(String description) {
+
         super(description);
     }
 
+    /**
+     * Returns the string representation of this todo task.
+     *
+     * @return String with [T] prefix, status icon, and description.
+     */
+    
     @Override
 
     public String toString() {
+
         return "[T]" + super.toString();
     }
 }
