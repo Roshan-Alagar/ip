91fb821b0f498e6b6303330ab22d03b2e974d689
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-03-03 18:53:28 +0800

    Add UI method to display search results

--- src/main/java/roshan/Ui.java
+++ src/main/java/roshan/Ui.java
@@ -1,5 +1,6 @@
 package roshan;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ui {
@@ -69,6 +70,16 @@ public class Ui {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + "." + tasks.get(i));
+        }
+    }
+    public void showFoundTasks(ArrayList<Task> foundTasks) {
+        if (foundTasks.isEmpty()) {
+            System.out.println("No matching tasks found.");
+        } else {
+            System.out.println("Here are the matching tasks in your list:");
+            for (int i = 0; i < foundTasks.size(); i++) {
+                System.out.println((i + 1) + "." + foundTasks.get(i));
+            }
         }
     }
 }
