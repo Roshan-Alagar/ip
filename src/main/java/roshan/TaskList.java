2f6d2ba11e527f9bbe9746249fa8430308fe4260
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-03-03 20:05:37 +0800

    Add JavaDoc comments to TaskList class

--- src/main/java/roshan/TaskList.java
+++ src/main/java/roshan/TaskList.java
@@ -2,36 +2,91 @@ package roshan;
 
 import java.util.ArrayList;
 
+/**
+ * Manages a list of tasks.
+ * Provides operations to add, delete, and retrieve tasks.
+ */
+
 public class TaskList {
     private ArrayList<Task> tasks;
+
+    /**
+     * Creates a new empty TaskList.
+     */
 
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Creates a new TaskList with the given list of tasks.
+     *
+     * @param tasks The initial list of tasks.
+     */
+
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
+
+    /**
+     * Adds a task to the list.
+     *
+     * @param task The task to add.
+     */
 
     public void add(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Removes a task from the list at the specified index.
+     *
+     * @param index The index of the task to remove (0-based).
+     * @return The removed task.
+     */
+
     public Task remove(int index) {
         return tasks.remove(index);
     }
+
+    /**
+     * Retrieves a task from the list at the specified index.
+     *
+     * @param index The index of the task to retrieve (0-based).
+     * @return The task at the specified index.
+     */
 
     public Task get(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The size of the task list.
+     */
+
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Returns the underlying ArrayList of tasks.
+     *
+     * @return The ArrayList containing all tasks.
+     */
+
     public ArrayList<Task> getTasks() {
         return tasks;
     }
+
+    /**
+     * Finds all tasks containing the specified keyword in their description.
+     *
+     * @param keyword The keyword to search for.
+     * @return ArrayList of tasks that match the keyword.
+     */
+
     public ArrayList<Task> find(String keyword) {
         ArrayList<Task> matchingTasks = new ArrayList<>();
         for (Task task : tasks) {
