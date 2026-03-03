e4f5339c84debc48eaa043a2c1f0d4af28c0f2f9
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-03-03 19:11:03 +0800

    Add JavaDoc comments to Deadline class and fixed Coding Standard

--- src/main/java/roshan/Deadline.java
+++ src/main/java/roshan/Deadline.java
@@ -1,16 +1,34 @@
 package roshan;
 
+/**
+ * Represents a task with a deadline.
+ */
+
 public class Deadline extends Task {
     protected String by;
+
+    /**
+     * Creates a new Deadline task with the given description and deadline.
+     *
+     * @param description The description of the deadline task.
+     * @param by The deadline date/time as a string.
+     */
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    /**
+     * Returns the string representation of this deadline task.
+     *
+     * @return String with [D] prefix, status, description, and deadline.
+     */
+
     @Override
 
     public String toString() {
+
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
