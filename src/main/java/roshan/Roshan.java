11a25733bd5e9ed7ed0ff1abcfc0729ed5ba6ab9
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-03-03 18:44:49 +0800

    Refactor Roshan to use Ui, Parser, and TaskList classes

--- src/main/java/roshan/Roshan.java
+++ src/main/java/roshan/Roshan.java
@@ -1,220 +1,128 @@
 package roshan;
 
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Roshan {
-    private static final String FILE_PATH = "./data/roshan.txt";
-    private static Storage storage;
-    private static ArrayList<Task> tasks;
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-    public static void main(String[] args) {
-        String logo = " ____            _                 \n"
-                + "|  _ \\ ___  ___| |__   __ _ _ __  \n"
-                + "| |_) / _ \\/ __| '_ \\ / _` | '_ \\ \n"
-                + "|  _ < (_) \\__ \\ | | | (_| | | | |\n"
-                + "|_| \\_\\___/|___/_| |_|\\__,_|_| |_|\n";
-        String line = " ____________________________________________________________";
-
-        System.out.println(logo);
-        System.out.println(line);
-        System.out.println("Hello I'm Roshan");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-
-        storage = new Storage(FILE_PATH);
-        tasks = new ArrayList<>();
-
-        // Load tasks from file
+    public Roshan(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
         try {
-            tasks = storage.loadTasks();
+            tasks = new TaskList(storage.loadTasks());
         } catch (RoshanException e) {
-            System.out.println("  " + e.getMessage());
-            System.out.println("  Starting with empty task list.");
+            ui.showLoadingError();
+            tasks = new TaskList();
         }
+    }
 
-        Scanner in = new Scanner(System.in);
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
 
-        while (true) {
+        while (!isExit) {
             try {
-                String input = in.nextLine();
-                System.out.println(line);
+                String input = ui.readCommand();
+                ui.showLine();
 
-                if (input.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    System.out.println(line);
+                String command = Parser.getCommand(input);
+
+                switch (command) {
+                case "bye":
+                    ui.showGoodbye();
+                    isExit = true;
                     break;
-                } else if (input.equals("list")) {
-                    handleList();
-                    System.out.println(line);
-                } else if (input.startsWith("mark ")) {
+                case "list":
+                    ui.showTaskList(tasks);
+                    break;
+                case "mark":
                     handleMark(input);
-                    System.out.println(line);
-                } else if (input.startsWith("unmark ")) {
+                    break;
+                case "unmark":
                     handleUnmark(input);
-                    System.out.println(line);
-                } else if (input.startsWith("delete ")) {
+                    break;
+                case "delete":
                     handleDelete(input);
-                    System.out.println(line);
-                } else if (input.startsWith("todo ")) {
+                    break;
+                case "todo":
                     handleTodo(input);
-                    System.out.println(line);
-                } else if (input.startsWith("deadline ")) {
+                    break;
+                case "deadline":
                     handleDeadline(input);
-                    System.out.println(line);
-                } else if (input.startsWith("event ")) {
+                    break;
+                case "event":
                     handleEvent(input);
-                    System.out.println(line);
-                } else if (input.trim().equals("todo") || input.trim().equals("deadline") || input.trim().equals("event")) {
-                    throw new RoshanException("OOPS!!! The description of a " + input.trim() + " cannot be empty.");
-                } else {
+                    break;
+                default:
+                    if (input.trim().equals("todo") || input.trim().equals("deadline")
+                            || input.trim().equals("event")) {
+                        throw new RoshanException("OOPS!!! The description of a "
+                                + input.trim() + " cannot be empty.");
+                    }
                     throw new RoshanException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             } catch (RoshanException e) {
-                System.out.println("  " + e.getMessage());
-                System.out.println(line);
+                ui.showError(e.getMessage());
             } catch (Exception e) {
-                System.out.println("  OOPS!!! Something went wrong: " + e.getMessage());
-                System.out.println(line);
+                ui.showError("OOPS!!! Something went wrong: " + e.getMessage());
+            } finally {
+                ui.showLine();
             }
         }
     }
 
-    private static void saveTasksToFile() {
-        try {
-            storage.saveTasks(tasks);
-        } catch (RoshanException e) {
-            System.out.println("  Warning: Could not save tasks - " + e.getMessage());
+    private void handleMark(String input) throws RoshanException {
+        int taskNumber = Parser.parseTaskNumber(input, 5);
+        if (taskNumber < 0 || taskNumber >= tasks.size()) {
+            throw new RoshanException("OOPS!!! Task number " + (taskNumber + 1) + " does not exist.");
         }
+        tasks.get(taskNumber).markAsDone();
+        ui.showTaskMarked(tasks.get(taskNumber));
+        storage.saveTasks(tasks.getTasks());
     }
 
-    private static void handleList() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + "." + tasks.get(i));
+    private void handleUnmark(String input) throws RoshanException {
+        int taskNumber = Parser.parseTaskNumber(input, 7);
+        if (taskNumber < 0 || taskNumber >= tasks.size()) {
+            throw new RoshanException("OOPS!!! Task number " + (taskNumber + 1) + " does not exist.");
         }
+        tasks.get(taskNumber).markAsNotDone();
+        ui.showTaskUnmarked(tasks.get(taskNumber));
+        storage.saveTasks(tasks.getTasks());
     }
 
-    private static void handleMark(String input) throws RoshanException {
-        String taskNumberStr = input.substring(5).trim();
-        if (taskNumberStr.isEmpty()) {
-            throw new RoshanException("OOPS!!! Please specify which task to mark.");
+    private void handleDelete(String input) throws RoshanException {
+        int taskNumber = Parser.parseTaskNumber(input, 7);
+        if (taskNumber < 0 || taskNumber >= tasks.size()) {
+            throw new RoshanException("OOPS!!! Task number " + (taskNumber + 1) + " does not exist.");
         }
-        try {
-            int taskNumber = Integer.parseInt(taskNumberStr) - 1;
-            if (taskNumber < 0 || taskNumber >= tasks.size()) {
-                throw new RoshanException("OOPS!!! Task number " + (taskNumber + 1) + " does not exist.");
-            }
-            tasks.get(taskNumber).markAsDone();
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println("  " + tasks.get(taskNumber));
-            saveTasksToFile();
-        } catch (NumberFormatException e) {
-            throw new RoshanException("OOPS!!! Please provide a valid task number.");
-        }
+        Task removedTask = tasks.remove(taskNumber);
+        ui.showTaskDeleted(removedTask, tasks.size());
+        storage.saveTasks(tasks.getTasks());
     }
 
-    private static void handleUnmark(String input) throws RoshanException {
-        String taskNumberStr = input.substring(7).trim();
-        if (taskNumberStr.isEmpty()) {
-            throw new RoshanException("OOPS!!! Please specify which task to unmark.");
-        }
-        try {
-            int taskNumber = Integer.parseInt(taskNumberStr) - 1;
-            if (taskNumber < 0 || taskNumber >= tasks.size()) {
-                throw new RoshanException("OOPS!!! Task number " + (taskNumber + 1) + " does not exist.");
-            }
-            tasks.get(taskNumber).markAsNotDone();
-            System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println("  " + tasks.get(taskNumber));
-            saveTasksToFile();
-        } catch (NumberFormatException e) {
-            throw new RoshanException("OOPS!!! Please provide a valid task number.");
-        }
+    private void handleTodo(String input) throws RoshanException {
+        Todo task = Parser.parseTodo(input);
+        tasks.add(task);
+        ui.showTaskAdded(task, tasks.size());
+        storage.saveTasks(tasks.getTasks());
     }
 
-    private static void handleDelete(String input) throws RoshanException {
-        String taskNumberStr = input.substring(7).trim();
-        if (taskNumberStr.isEmpty()) {
-            throw new RoshanException("OOPS!!! Please specify which task to delete.");
-        }
-        try {
-            int taskNumber = Integer.parseInt(taskNumberStr) - 1;
-            if (taskNumber < 0 || taskNumber >= tasks.size()) {
-                throw new RoshanException("OOPS!!! Task number " + (taskNumber + 1) + " does not exist.");
-            }
-            Task removedTask = tasks.remove(taskNumber);
-            System.out.println("Noted. I've removed this task:");
-            System.out.println("  " + removedTask);
-            System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-            saveTasksToFile();
-        } catch (NumberFormatException e) {
-            throw new RoshanException("OOPS!!! Please provide a valid task number.");
-        }
+    private void handleDeadline(String input) throws RoshanException {
+        Deadline task = Parser.parseDeadline(input);
+        tasks.add(task);
+        ui.showTaskAdded(task, tasks.size());
+        storage.saveTasks(tasks.getTasks());
     }
 
-    private static void handleTodo(String input) throws RoshanException {
-        String description = input.substring(5).trim();
-        if (description.isEmpty()) {
-            throw new RoshanException("OOPS!!! The description of a todo cannot be empty.");
-        }
-        Task newTask = new Todo(description);
-        tasks.add(newTask);
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + newTask);
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-        saveTasksToFile();
+    private void handleEvent(String input) throws RoshanException {
+        Event task = Parser.parseEvent(input);
+        tasks.add(task);
+        ui.showTaskAdded(task, tasks.size());
+        storage.saveTasks(tasks.getTasks());
     }
 
-    private static void handleDeadline(String input) throws RoshanException {
-        String commandContent = input.substring(9).trim();
-        if (commandContent.isEmpty()) {
-            throw new RoshanException("OOPS!!! The description of a deadline cannot be empty.");
-        }
-        int byIndex = commandContent.indexOf("/by");
-        if (byIndex == -1) {
-            throw new RoshanException("OOPS!!! Please specify the deadline using /by.");
-        }
-        String description = commandContent.substring(0, byIndex).trim();
-        String by = commandContent.substring(byIndex + 3).trim();
-        if (description.isEmpty()) {
-            throw new RoshanException("OOPS!!! The description of a deadline cannot be empty.");
-        }
-        if (by.isEmpty()) {
-            throw new RoshanException("OOPS!!! Please specify when the deadline is.");
-        }
-        Task newTask = new Deadline(description, by);
-        tasks.add(newTask);
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + newTask);
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-        saveTasksToFile();
-    }
-
-    private static void handleEvent(String input) throws RoshanException {
-        String commandContent = input.substring(6).trim();
-        if (commandContent.isEmpty()) {
-            throw new RoshanException("OOPS!!! The description of an event cannot be empty.");
-        }
-        int fromIndex = commandContent.indexOf("/from");
-        int toIndex = commandContent.indexOf("/to");
-        if (fromIndex == -1 || toIndex == -1) {
-            throw new RoshanException("OOPS!!! Please specify the event time using /from and /to.");
-        }
-        String description = commandContent.substring(0, fromIndex).trim();
-        String from = commandContent.substring(fromIndex + 5, toIndex).trim();
-        String to = commandContent.substring(toIndex + 3).trim();
-        if (description.isEmpty()) {
-            throw new RoshanException("OOPS!!! The description of an event cannot be empty.");
-        }
-        if (from.isEmpty() || to.isEmpty()) {
-            throw new RoshanException("OOPS!!! Please specify when the event starts and ends.");
-        }
-        Task newTask = new Event(description, from, to);
-        tasks.add(newTask);
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + newTask);
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-        saveTasksToFile();
+    public static void main(String[] args) {
+        new Roshan("./data/roshan.txt").run();
     }
 }
